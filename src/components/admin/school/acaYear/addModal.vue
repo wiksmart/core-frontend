3e81d9ad29279d078dd02f7f2641046004fc190f
Richal Zacky <richalzackyi@gmail.com>
2023-02-13 14:53:00 +0700

    tambahin menu add

--- src/components/admin/school/acaYear/addModal.vue
+++ src/components/admin/school/acaYear/addModal.vue
@@ -5,13 +5,38 @@
         </div>
 
         <div class="modal-value">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. At in iure iste ipsam perspiciatis, debitis molestiae ad fugit iusto! Quos neque impedit eveniet, dignissimos enim facilis consectetur temporibus placeat asperiores.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. At in iure iste ipsam perspiciatis, debitis molestiae ad fugit iusto! Quos neque impedit eveniet, dignissimos enim facilis consectetur temporibus placeat asperiores.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. At in iure iste ipsam perspiciatis, debitis molestiae ad fugit iusto! Quos neque impedit eveniet, dignissimos enim facilis consectetur temporibus placeat asperiores.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. At in iure iste ipsam perspiciatis, debitis molestiae ad fugit iusto! Quos neque impedit eveniet, dignissimos enim facilis consectetur temporibus placeat asperiores.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. At in iure iste ipsam perspiciatis, debitis molestiae ad fugit iusto! Quos neque impedit eveniet, dignissimos enim facilis consectetur temporibus placeat asperiores.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. At in iure iste ipsam perspiciatis, debitis molestiae ad fugit iusto! Quos neque impedit eveniet, dignissimos enim facilis consectetur temporibus placeat asperiores.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. At in iure iste ipsam perspiciatis, debitis molestiae ad fugit iusto! Quos neque impedit eveniet, dignissimos enim facilis consectetur temporibus placeat asperiores.
+            <div class="row-input">
+                <div class="input-data half-input">
+                    <div class="title-input"><p>{{ $t('tableAcaYear.startYear') }}</p></div>
+
+                    <div><input type="date"></div>
+                </div>
+
+                <div class="input-data half-input">
+                    <div class="title-input"><p>{{ $t('tableAcaYear.endYear') }}</p></div>
+
+                    <div><input type="date"></div>
+                </div>
+            </div>
+
+            <div class="row-input">
+                <div class="input-data half-input">
+                    <div class="title-input"><p>{{ $t('tableAcaYear.desc') }}</p></div>
+
+                    <div><input type="text" :placeholder="$t('tableAcaYear.addStart')"></div>
+                </div>
+
+                <div class="input-data half-input">
+                    <div class="title-input"><p>{{ $t('tableAcaYear.status') }}</p></div>
+
+                    <div class="act-tog">
+                        <div class="tog-2" @click="isActive">
+                            <div class="btn-tog-2" :class="isActive ? 'tog-rg' : 'tog-lf'"></div>
+                        </div>
+                        <p>Active</p>
+                    </div>
+                </div>
+            </div>
         </div>
 
         <div class="modal-action">
@@ -30,8 +55,11 @@
                 emit("cancel-click", event, item)
             }
 
+            const isActive = ref(false)
+
             return {
-                cancelClick
+                cancelClick,
+                isActive
             }
         },
     }
